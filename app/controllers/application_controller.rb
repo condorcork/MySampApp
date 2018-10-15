0bb444aae4aaf2387e0058541e29029ae9becb2e
condor san <condordonkey@gmail.com>
2018-10-15 15:03:56 +0900

    with a litte check

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
+# coding: utf-8
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  def hello
+     render html: "hello, world!"
+    # render :html => "hello, wold!"
+    # render :file  => "/public/404.html"
+    #
+  end
+
+
+  def bye
+    render html: "bye!"
+  end
+
 end
